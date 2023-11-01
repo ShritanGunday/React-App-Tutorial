6e06648494bfb33610407e5dfdd73ea21fd2270f
Shritan Gunday <113409877+ShritanGunday@users.noreply.github.com>
2023-10-31 23:05:57 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+import React from "react"
+
+const element = document.createElement("h2"); //Creates HTML H2 Element
+element.className = "container center"; //Two different classes in one line
+element.style = "color: red; background-color: blue"; //Sets stylization
