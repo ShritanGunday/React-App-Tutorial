ad42e8f5ad3af9efc90dd9e80b4d448b03387bc0
Shritan Gunday <gunday.1@buckeyemail.osu.edu>
2023-11-01 12:39:03 -0400

    Introduced react-dom element and roots

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-import React from "react"
+import React from "react";
+import {createRoot} from "react-dom/client";
 
-const element = React.createElement("h1", {className:"class", style:"color: red"}, "Hello World");
+//Create React element with h1 size, printing string
+const element = React.createElement("h1", {}, "Hello World");
+
+//Creates a reference to the root element from the page
+const root = document.querySelector("#react-root");
+//Creates the root and renders the passed React element to the screen
+createRoot(root).render(element);
